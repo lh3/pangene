2648968c549e5b67d383b2af5d76fd3a1a9b9977
Heng Li <lh3@me.com>
2023-10-24 21:29:05 -0400

    Release pangene-1.0 (r183)

--- cmd-log.sh
+++ cmd-log.sh
@@ -37,3 +37,20 @@ pangene -P Mtb.H37Rv.txt -p.001 aln/*.paf.gz 2> Mtb-merge-p0.r177.gfa.log | gzip
 # run panaroo
 runlog panaroo -i *.gff -o panaroo.strict -t 16 --clean-mode strict > panaroo.strict.log 2>&1
 runlog panaroo -i *.gff -o panaroo.strict-merge -t 16 --clean-mode strict --merge_paralogs > panaroo.strict-merge.log 2>&1
+
+#############
+### Human ###
+#############
+
+ls *.fa.gz | sed s,.fa.gz,, | xargs -i echo miniprot --outs=0.97 --no-cs -Iut16 {}.fa.gz ../HUMAN.faa.gz \| gzip \> {}.paf.gz|asub -j run-mp -M36 -n16
+../pangene -a2 -I@gape.huamn.incl -X@gape.human.excl aln-human/[14]*.paf.gz 2> human-human98-r182.gfa.log | gzip > human-human98-r182.gfa.gz
+
+##################
+### Great apes ###
+##################
+
+# generate proteins
+cat HUMAN.faa.gz PANTR.faa.gz PANPA.faa.gz GROGO.faa.gz PONAB.faa.gz > merged.faa.gz
+ls *.fa.gz|sed s,.fa.gz,,|xargs -i echo miniprot --outs=0.97 --no-cs -Iut16 {}.fa.gz merged.faa.gz \| gzip \> {}.paf.gz | asub -j run-mp -M40 -n16
+../pangene 1100_GRCh38.0.paf.gz 7031_chimpanzee.paf.gz 7032_bonobo.paf.gz 7033_gorilla.paf.gz 7034_orangutan.paf.gz > 5a.gfa
+(zcat ../proteins/HUMAN.faa.gz; seqtk comp merged.faa.gz|cut -f1|grep -wFf <(../pangene.js gfa2matrix 5a.gfa|grep _|awk '$2==0'|cut -f1) | seqtk subseq -l80 merged.faa.gz -) | gzip > human+other.faa.gz
